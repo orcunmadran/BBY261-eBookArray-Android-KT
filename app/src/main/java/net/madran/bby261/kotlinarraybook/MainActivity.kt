6ebf1a2078251a536a02560969203a61253ab81e
Orçun Madran <orcunmadran@gmail.com>
2017-11-17 15:31:48 +0300

    Ön Hazırlık

---
+++ app/src/main/java/net/madran/bby261/kotlinarraybook/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.madran.bby261.kotlinarraybook
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
