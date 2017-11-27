eaeb0405b3c5a0b0dddf7a063e6dee022329417c
Orçun Madran <orcunmadran@gmail.com>
2017-11-27 14:47:05 +0300

    Uygulama Hazır

--- app/src/main/java/net/madran/bby261/kotlinarraybook/MainActivity.kt
+++ app/src/main/java/net/madran/bby261/kotlinarraybook/MainActivity.kt
@@ -2,6 +2,9 @@ package net.madran.bby261.kotlinarraybook
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,14 +13,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        when (1) {
-            1 -> kitapGorsel.setImageResource(R.drawable.sayfa_01)
-            2 -> kitapGorsel.setImageResource(R.drawable.kitap_gorsel)
-            else -> {
+        var sayfa = 1
+        var sayfalar = arrayOf(R.drawable.sayfa01, R.drawable.sayfa02, R.drawable.sayfa03, R.drawable.sayfa04, R.drawable.sayfa05)
 
+        ileriButon.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(view: View): Unit {
+                if(sayfa < sayfalar.size) {
+                    sayfa++
+                    Log.d("Dizi", sayfalar[sayfa-1].toString())
+                    sayfaTutucu.setImageResource(sayfalar[sayfa-1])
+                } else {
+                    Toast.makeText(this@MainActivity, "Son sayfadasınız..!",
+                            Toast.LENGTH_LONG).show()
+                }
             }
+        })
 
-        }
+        geriButon.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(view: View): Unit {
+                if(sayfa > 1) {
+                    sayfa--
+                    Log.d("Dizi", sayfalar[sayfa-1].toString())
+                    sayfaTutucu.setImageResource(sayfalar[sayfa-1])
+                } else {
+                    Toast.makeText(this@MainActivity, "İlk sayfadasınız..!",
+                            Toast.LENGTH_LONG).show()
+                }
+            }
+        })
 
     }
 
