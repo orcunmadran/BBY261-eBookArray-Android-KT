1bd2680a34e77f66c1730ba9002c9a612cf6eff9
Orçun Madran <orcunmadran@gmail.com>
2017-12-01 15:19:53 +0300

    Metin dizisi eklendi

--- app/src/main/java/net/madran/bby261/kotlinarraybook/MainActivity.kt
+++ app/src/main/java/net/madran/bby261/kotlinarraybook/MainActivity.kt
@@ -15,6 +15,8 @@ class MainActivity : AppCompatActivity() {
 
         var sayfa = 1
         var sayfalar = arrayOf(R.drawable.sayfa01, R.drawable.sayfa02, R.drawable.sayfa03, R.drawable.sayfa04, R.drawable.sayfa05)
+        var metinler = arrayOf("Metin 01", "Metin 02", "Metin 03", "Metin 04", "Metin 05")
+        metinTutucu.setText(metinler[0])
 
         ileriButon.setOnClickListener(object: View.OnClickListener {
             override fun onClick(view: View): Unit {
@@ -22,6 +24,7 @@ class MainActivity : AppCompatActivity() {
                     sayfa++
                     Log.d("Dizi", sayfalar[sayfa-1].toString())
                     sayfaTutucu.setImageResource(sayfalar[sayfa-1])
+                    metinTutucu.setText(metinler[sayfa-1])
                 } else {
                     Toast.makeText(this@MainActivity, "Son sayfadasınız..!",
                             Toast.LENGTH_LONG).show()
@@ -35,6 +38,7 @@ class MainActivity : AppCompatActivity() {
                     sayfa--
                     Log.d("Dizi", sayfalar[sayfa-1].toString())
                     sayfaTutucu.setImageResource(sayfalar[sayfa-1])
+                    metinTutucu.setText(metinler[sayfa-1])
                 } else {
                     Toast.makeText(this@MainActivity, "İlk sayfadasınız..!",
                             Toast.LENGTH_LONG).show()
